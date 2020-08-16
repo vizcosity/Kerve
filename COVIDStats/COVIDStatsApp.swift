60fa0497566531e2d51c46273089a6939c53855b
Aaron Baw <aaronbaw@gmail.com>
2020-08-16 15:47:00 +0200

    Initial Commit

---
+++ COVIDStats/COVIDStatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  COVIDStatsApp.swift
+//  COVIDStats
+//
+//  Created by Aaron Baw on 16/08/2020.
+//
+
+import SwiftUI
+
+@main
+struct COVIDStatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ COVIDStats/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  COVIDStats
+//
+//  Created by Aaron Baw on 16/08/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
